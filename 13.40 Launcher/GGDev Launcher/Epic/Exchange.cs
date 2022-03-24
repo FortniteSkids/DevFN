1e43e6dfb19a652d116dc754721fba3887e7e9a5
John Shier <79332140+Array0x@users.noreply.github.com>
2022-03-24 09:36:42 -0400

    Add files via upload

---
+++ 13.40 Launcher/GGDev Launcher/Epic/Exchange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace GGDev_Launcher.Epic
+{
+	public class Exchange
+	{
+		[JsonPropertyName("expiresInSeconds")]
+		public int ExpiresInSeconds { get; set; }
+
+		[JsonPropertyName("code")]
+		public string Code { get; set; }
+
+		[JsonPropertyName("creatingClientId")]
+		public string CreatingClientId { get; set; }
+	}
+}
