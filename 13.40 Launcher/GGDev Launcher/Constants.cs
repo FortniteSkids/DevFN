698e09556cf2aac59f3aaf649547a30287478707
John Shier <79332140+Array0x@users.noreply.github.com>
2022-03-24 09:35:31 -0400

    Add files via upload

---
+++ 13.40 Launcher/GGDev Launcher/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGDev_Launcher
+{
+	// Token: 0x0200000B RID: 11
+	internal class Constants
+	{
+		// Token: 0x04000015 RID: 21
+		public static string InLobby = "LogHotfixManager: Display: Update State CheckingForPatch -> CheckingForHotfix";
+	}
+}
+
+
