765abdc7737b7b6828a72ee6fec5da092b68b695
John Shier <79332140+Array0x@users.noreply.github.com>
2022-03-25 18:48:05 -0400

    Add files via upload

---
+++ Backup Backend/main.py
@@ -0,0 +1,12 @@
+from files.workers.api import *
+port = 6969
+for key in db.keys():
+ del db[key]
+ 
+@app.exception(sanic.exceptions.NotFound)
+async def ignore_404s(request, exception):
+    return sanic.response.json("ok")
+
+
+print('\033[30;1m')
+app.run(host="0.0.0.0", port=port)
